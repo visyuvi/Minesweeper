e4a8964a5f071b653a0f5e0a1b9598e774bd4234
Your Name <you@example.com>
2023-07-10 21:37:43 +0530

    Code to display value at a square when clicked(making the square uncovered)

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ pygame.display.set_caption("Minesweeper")
 BG_COLOR = "white"
 ROWS, COLS = 15, 15
 MINES = 15
+SIZE = WIDTH / ROWS
+
 RECT_COLOR = (200, 200, 200)
 CLICKED_RECT_COLOR = (140, 140, 140)
 
@@ -76,26 +78,31 @@ def create_mine_field(rows, cols, mines):
 def draw(win, field, cover_field):
     win.fill(BG_COLOR)
 
-    size = WIDTH / ROWS
-
     for i, row in enumerate(field):
-        y = size * i
+        y = SIZE * i
         for j, value in enumerate(row):
-            x = size * j
+            x = SIZE * j
 
             is_covered = cover_field[i][j] == 0
             if is_covered:
-                pygame.draw.rect(win, RECT_COLOR, (x, y, size, size))
-                pygame.draw.rect(win, "black", (x, y, size, size), 3)
+                pygame.draw.rect(win, RECT_COLOR, (x, y, SIZE, SIZE))
+                pygame.draw.rect(win, "black", (x, y, SIZE, SIZE), 3)
                 continue
             else:
-                pygame.draw.rect(win, CLICKED_RECT_COLOR, (x, y, size, size))
-                pygame.draw.rect(win, "black", (x, y, size, size), 3)
+                pygame.draw.rect(win, CLICKED_RECT_COLOR, (x, y, SIZE, SIZE))
+                pygame.draw.rect(win, "black", (x, y, SIZE, SIZE), 3)
 
             if value > 0:
                 text = NUM_FONT.render(str(value), 1, NUM_COLORS[value])
-                win.blit(text, (x + (size/2 - text.get_width()/2), y + (size/2 - text.get_height()/2)))
+                win.blit(text, (x + (SIZE / 2 - text.get_width() / 2), y + (SIZE / 2 - text.get_height() / 2)))
     pygame.display.update()
+
+
+def get_grid_pos(mouse_pos):
+    mx, my = mouse_pos
+    row = int(my // SIZE)
+    col = int(mx // SIZE)
+    return row, col
 
 
 def main():
@@ -108,6 +115,14 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                row, col = get_grid_pos(pygame.mouse.get_pos())
+
+                if row >= ROWS or col >= COLS:
+                    continue
+
+                cover_field[row][col] = 1
 
         draw(win, field, cover_field)
     pygame.quit()
